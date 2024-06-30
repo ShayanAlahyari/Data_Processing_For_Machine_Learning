46e9b2da39309bbb1963bd91562f0025d7dfaf91
shayanalahyari <shayanforstock@gmail.com>
2024-06-30 22:29:20 +0330

    ...

--- practice.py
+++ practice.py
@@ -1,3 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+dataset = pd.read_csv(filepath_or_buffer='Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
