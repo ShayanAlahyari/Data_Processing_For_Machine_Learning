ab71de45dd1d81e5e18dbb061d841e6eb4958526
shayanalahyari <shayanforstock@gmail.com>
2024-07-01 18:55:08 +0330

    ...

--- practice.py
+++ practice.py
@@ -16,4 +16,19 @@ from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
 imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
-print(x)
+
+###################
+
+# Handling categorical data
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+###########
