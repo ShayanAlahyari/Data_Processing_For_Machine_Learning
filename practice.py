9a4ea896663920b841c78e1ff0a7ecc52d122ad1
shayanalahyari <shayanforstock@gmail.com>
2024-07-02 11:02:14 +0330

    ...

--- practice.py
+++ practice.py
@@ -1,34 +1,36 @@
 # importing libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+
 ###############################
 
 # creating our dataset and creating matrix of features and the output column
 dataset = pd.read_csv(filepath_or_buffer='Data.csv')
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:,-1].values
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 ##################################
 
 # handling missing data
-from sklearn.impute import SimpleImputer
 
-imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
-imputer.fit(x[:,1:3])
-x[:,1:3] = imputer.transform(x[:,1:3])
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 ###################
 
 # Handling categorical data
 
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-
-ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
 
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 
 ###########
+
+
+# Splitting dataset into training set and test set before feature scaling
